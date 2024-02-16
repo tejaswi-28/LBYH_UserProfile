db407fcf39ae7572d0cd7de7ccb98ef5e6fefa69
Tejaswi Gaikwad <103772947+tejaswi-28@users.noreply.github.com>
2024-02-16 19:53:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    var x = document.getElementById("key1");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+}
+function myFunction2() {
+    var x = document.getElementById("key2");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+}
+function myFunction3() {
+    var x = document.getElementById("key3");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+}
